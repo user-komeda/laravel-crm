54c3bd1ec027ba7aff724c90c9e82138654cbbe8
DESKTOP-S09O801\shigo <shigoto922@gmail.com>
2021-06-02 21:42:50 +0900

    #3 email認証カスタマイズ
    メールサーバの設定を追加
    UserModelの変更

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->middleware(['auth'])->name('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 require __DIR__ . '/auth.php';
